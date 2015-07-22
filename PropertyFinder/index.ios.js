1a7ec4ecc05f24c82312d3cce049147e4c4fafaa
greatabel <greatabel1@126.com>
2015-07-22 16:40:53 +0800

    property finder

---
+++ PropertyFinder/index.ios.js
@@ -0,0 +1,66 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var PropertyFinder = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+
+});
+
+class PropertyFinderApp extends React.Component{
+  render() {
+    return React.createElement(React.Text , {style: styles.text}, "Hello World!");
+  }
+}
+AppRegistry.registerComponent('PropertyFinder', function() {return PropertyFinderApp});
+// AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
