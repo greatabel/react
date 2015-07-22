5178a99979f2b650d214059be59fe5382ec4a0ab
greatabel <greatabel1@126.com>
2015-07-22 23:03:07 +0800

    detail and list

--- PropertyFinder/SearchResults.js
+++ PropertyFinder/SearchResults.js
@@ -2,6 +2,7 @@
  
 var React = require('react-native');
 
+var PropertyView = require('./PropertyView');
 
 var {
   StyleSheet,
@@ -12,6 +13,34 @@ var {
   Text,
   Component
 } = React;
+
+var styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
 
 class SearchResults extends Component{
 
@@ -24,12 +53,33 @@ class SearchResults extends Component{
     };
   }
 
+  rowPressed(propertyGuid){
+    var property = this.props.listings
+      .filter(prop => prop.guid === propertyGuid)[0];
+
+    this.props.navigator.push({
+      title: "Property",
+      component: PropertyView,
+      passProps: {property: property}
+    });
+  }
+
   renderRow(rowData, sectionID, rowID) {
+    var price = rowData.price_formatted.split(' ')[0];
+
     return (
-      <TouchableHighlight
+      <TouchableHighlight onPress={() => this.rowPressed(rowData.guid)}
           underlayColor='#dddddd'>
         <View>
-          <Text>{rowData.title}</Text>
+          <View style={styles.rowContainer}>
+            <Image style={styles.thumb} source={{ uri: rowData.img_url }} />
+            <View  style={styles.textContainer}>
+              <Text style={styles.price}>£{price}</Text>
+              <Text style={styles.title} 
+                    numberOfLines={1}>{rowData.title}</Text>
+            </View>
+          </View>
+          <View style={styles.separator}/>
         </View>
       </TouchableHighlight>
     );
