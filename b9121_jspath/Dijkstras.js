542961a1f770c251aa134f074617fd46baf17e65
greatabel <myreceiver2for2github@gmail.com>
2021-01-17 23:37:23 +0800

    b9121

---
+++ b9121_jspath/Dijkstras.js
@@ -0,0 +1,49 @@
+var Dijkstras = (function () {
+
+    var Dijkstras = function () {
+        this.graph = [];
+        this.queue;
+        this.distance = [];
+        this.previous = []
+    }
+
+
+    Dijkstras.prototype.settingGraph = function (graph)
+    {
+        // Error check graph
+        if (typeof graph !== 'object') {
+            throw "graph isn't an object (" + typeof graph + ")";
+        }
+
+        if (graph.length < 1) {
+            throw "graph is empty";
+        }
+
+        for (var index in graph) {
+            // Error check each node
+            var node = graph[index];
+            if (typeof node !== 'object' || node.length !== 2) {
+                throw "node must be an array and contain 2 values (name, vertices). Failed at index: " + index;
+            }
+
+            var nodeName = node[0];
+            var vertices = node[1];
+            this.graph[nodeName] = [];
+
+            for (var v in vertices) {
+                // Error check each node
+                var vertex = vertices[v];
+                if (typeof vertex !== 'object' || vertex.length !== 2) {
+                    throw "vertex must be an array and contain 2 values (name, vertices). Failed at index: " + index + "[" + v + "]" ;
+                }
+                var vertexName = vertex[0];
+                var vertexCost = vertex[1];
+                this.graph[nodeName][vertexName] = vertexCost;
+            }
+        }
+    }
+
+
+
+    return Dijkstras;
+})();
