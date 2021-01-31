005b2a67768a9373b2c8fb7c8b72104f550dd927
greatabel <myreceiver2for2github@gmail.com>
2021-02-01 01:59:27 +0800

    new html folder

--- b9121_jspath/jspath/App.js
+++ b9121_jspath/jspath/App.js
@@ -4,12 +4,14 @@ import { Text, View } from 'react-native';
 import React, { Component } from 'react';
 import { WebView } from 'react-native-webview';
 
+const combinedHTML = require('./combined.html')
 const HelloWorldApp = () => {
   return (
      <WebView
         originWhitelist={['*']}
-        source={{ html: '<br/><h1>This is a static HTML source!</h1>'+
-        '<body><h1>test</h1></body>' }}
+        // source={{ html: '<br/><h1>This is a static HTML source!</h1>'+
+        // '<body><h1>test</h1></body>' }}
+        source={combinedHTML}
       />
 
   )
