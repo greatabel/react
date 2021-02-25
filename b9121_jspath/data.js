00dd51581f09e98b402c0704a17c34df537b858c
greatabel <myreceiver2for2github@gmail.com>
2021-02-25 14:27:41 +0800

    load data.js from outside

--- b9121_jspath/data.js
+++ b9121_jspath/data.js
@@ -1,6 +1,7 @@
 // var DATA = {"a" : "b", "c" : "d"};
 
 var Instructions_Dic = new Array();
+var Connections_Dic = new Array();
 
 //定义一个字典 存储边对应的 提示操作语句
 // path_instruction_dic0 中的0 代表是 placeid=0的提示语
@@ -22,11 +23,7 @@ path_instruction_dic1['C_B'] = 'Go straight along the aisle and turn right when 
 path_instruction_dic1['B_A'] = 'Keep walking until you see the rest table.'
 path_instruction_dic1['D_C'] = 'Take the stairs upstairs to the upper floor.'
 
-Instructions_Dic[0] = path_instruction_dic0;
-Instructions_Dic[1] = path_instruction_dic1;
 
-
-var Connections_Dic = new Array();
 
 var connection0 = [
             ['A', [['B', 20], ['D', 150]] ], 
@@ -42,6 +39,9 @@ var connection1 = [
             ['C', [['D', 10] ] ], 
             ['D', [['C', 10] ]]
         ];
-        
+
+Instructions_Dic[0] = path_instruction_dic0;
+Instructions_Dic[1] = path_instruction_dic1;
+
 Connections_Dic[0] = connection0;
 Connections_Dic[1] = connection1;
--- b9121_jspath/readme.md
+++ b9121_jspath/readme.md
@@ -5,3 +5,6 @@ https://www.instamobile.io/react-native-tutorials/react-native-webview/
 https://www.jianshu.com/p/121cbe699478
 
 https://blog.csdn.net/darkLight2017/article/details/107413139
+
+https://cdn.jsdelivr.net/gh/greatabel/react/b9121_jspath/jspath/data.js
+配置data.js 需要从外界加载，需要用户自己先构建一个自己的github
