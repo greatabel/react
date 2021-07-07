8867f9ffff3172d6c7e1109f2a6d58f70cdabde4
greatabel <myreceiver2for2github@gmail.com>
2021-07-07 16:37:55 +0800

    i00react-native-in-action

--- a13127_rn_mysql/BadDateFormProject/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import YourApp from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => YourApp);
