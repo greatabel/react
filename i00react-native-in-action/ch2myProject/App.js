ce5da5beb1ea667af2587eae62fd156c7502ff39
greatabel <myreceiver2for2github@gmail.com>
2021-07-09 11:22:12 +0800

    ch2

--- i00react-native-in-action/ch2myProject/App.js
+++ i00react-native-in-action/ch2myProject/App.js
@@ -11,6 +11,13 @@ export default class MyComponent extends Component<{}> {
     super()
     this.state = {
     year: 2016,
+    leapYear: true,
+    topics: ['React', 'React Native', 'JavaScript'],
+    info: {
+      papaerback: true,
+      length: '335 pages',
+      type: 'programming'
+    },
     name: 'Nader Dabit', 
     colors: ['blue']
     }
@@ -22,16 +29,30 @@ export default class MyComponent extends Component<{}> {
     })
   }
 
+  update(){
+    this.forceUpdate()
+  }
 
   render() {
+    let leapYear = <Text>This is not a leapYear! </Text>
+    if (this.state.leapYear) {
+      leapYear = <Text>This is a leapYear!</Text>
+    }
     return (
     <View>
+    <Text></Text>
     <Text></Text>
     <Text></Text>
     <Text></Text>
     <Text>My name is: { this.state.name }</Text>
     <Text onPress={()=> this.updateYear()} >The year is: { this.state.year }</Text>
     <Text>My colors are { this.state.colors[0] }</Text> 
+    <Text onPress={ ()=> this.update()} >Force Update</Text> 
+    <Text></Text>
+    <Text></Text>
+    <Text>Length: { this.state.info.length }</Text>
+    <Text>Type: { this.state.info.type }</Text>
+    { leapYear }
     </View>
       ) 
   }
