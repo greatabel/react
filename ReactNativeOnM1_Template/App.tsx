a8cbcfb6f54fa06f0c3dfe3fa44d3bdb09861179
greatabel <myreceiver2for2github@gmail.com>
2022-01-11 19:44:09 +0800

    backup ReactNativeOnM1_Template

--- ReactNativeOnM1_Template/App.tsx
+++ ReactNativeOnM1_Template/App.tsx
@@ -68,25 +68,25 @@ const App = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-
+        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Receiving">
+          <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
-          <Section title="Product Movement">
+          <Section title="See Your Changes">
             <ReloadInstructions />
           </Section>
-          <Section title="Add Lot FGLP or BIN">
+          <Section title="Debug">
             <DebugInstructions />
           </Section>
-          <Section title="Audit">
+          <Section title="Learn More">
             Read the docs to discover what to do next:
           </Section>
-
+          <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
