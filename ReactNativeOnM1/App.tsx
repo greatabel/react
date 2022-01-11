3e560ca1d4bd175b342a3180c1b1d891f7b24274
greatabel <myreceiver2for2github@gmail.com>
2022-01-11 20:58:55 +0800

    jump screen

--- ReactNativeOnM1/App.tsx
+++ ReactNativeOnM1/App.tsx
@@ -1,115 +1,82 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  AppRegistry,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  Button,
+  Alert
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+
+
+
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="ProfileBIN" component={ProfileBINScreen} />
+        <Stack.Screen name="ProfileLotCode" component={ProfileLotCodeScreen} />
+      </Stack.Navigator>
+
+
+    </NavigationContainer>
+
+
+  );
+};
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+   
+    <Button
+      title="Build FG Pallet"
+      onPress={() =>
+        navigation.navigate('Profile', { name: 'FG' })
+      }
+    />
+    <Button
+      title="Build BIN"
+      onPress={() =>
+        navigation.navigate('ProfileBIN', { name: 'BIN' })
+      }
+    />
+    <Button
+      title="Audit Lot Code"
+      onPress={() =>
+        navigation.navigate('ProfileLotCode', { name: 'LotCode' })
+      }
+    />
     </View>
   );
 };
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Receiving">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="Product Movement">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Add Lot FGLP or BIN">
-            <DebugInstructions />
-          </Section>
-          <Section title="Audit">
-            Read the docs to discover what to do next:
-          </Section>
-
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+const ProfileScreen = ({ navigation, route }) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const ProfileBINScreen = ({ navigation, route }) => {
+  return <Text>This is {route.params.name}'s ProfileBINScreen</Text>;
+};
 
-export default App;
+const ProfileLotCodeScreen = ({ navigation, route }) => {
+  return <Text>This is {route.params.name}'s ProfileBINScreen</Text>;
+};
+
+
+export default MyStack;
